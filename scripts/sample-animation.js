4ce2e80ff2df9d68cb10ec0bb474460ea5c41f68
Troy Feng <troy.feng@yale.edu>
2020-08-24 17:35:50 -0400

    Playing around with basic GSAP

--- scripts/sample-animation.js
+++ scripts/sample-animation.js
@@ -1,3 +1,30 @@
-document.getElementById("testContainer").innerHTML = "GSAP";
+gsap.to("#testContainer", {
+	duration: 1, 
+	rotation: 360,
+	ease: "none",
+	repeat:-1
+});
 
-gsap.to("#testContainer", {duration: 1, rotation: 360});
+const centerColors = [
+	"rgb(0,120,255)",
+	"rgb(0,0,255)",
+	"rgb(120,0,255)",
+	"rgb(255,0,255)",
+	"rgb(255,0,120)",
+	"rgb(255,0,0)",
+	"rgb(255,120,0)",
+	"rgb(255,255,0)",
+	"rgb(120,255,0)",
+	"rgb(0,255,0)",
+	"rgb(0,255,120)",
+	"rgb(0,255,255)"];
+const centerTL = gsap.timeline({
+	repeat:-1,
+	repeatRefresh:true
+});
+centerColors.forEach((c) => {
+	centerTL.to("#centerMenu", {
+		color:c,
+		duration:1
+	});
+});
