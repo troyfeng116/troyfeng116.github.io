4c00f07f2b99ca8b88b333fde0340ec67369d90c
Troy Feng <troy.feng@yale.edu>
2020-10-10 21:24:25 -0400

    Imported media, refactored Projects

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -1,78 +1,72 @@
 import './Projects.css'
 import { FaGithub } from 'react-icons/fa'
 import React from 'react'
+import NQueensDemo from '../Media/Images/n-queens-demo.png'
+import KnightsTourDemo from "../Media/Images/knights-tour-demo.png"
+import SortingVisualizerDemo from "../Media/Images/sorting-visualizer-demo-img.png"
 
 export const Projects = () => {
+	const urls = [
+		"https://troyfeng116.github.io/chess-backtracking/index.html",
+		"https://troyfeng116.github.io/sorting-visualizer",
+		"https://troyfeng116.github.io/matrix-operations/index.html",
+		"",
+		"",
+	]
+	const titles = [
+		"Chess Backtracking Visualizers",
+		"Sorting Visualizer",
+		"Matrix Operations Calculator",
+		"Project Euler+",
+		"Daily Coding Problem",
+	]
+	const imgs = [
+		NQueensDemo,
+		SortingVisualizerDemo,
+		SortingVisualizerDemo,
+		SortingVisualizerDemo,
+		SortingVisualizerDemo,
+	]
+	const subtext = [
+		"A Vanilla JavaScript interactive visualizer for the classic N Queens and Knight's Tour puzzles.",
+		"A React+D3 Typescript web app visualizer for common sorting algorithms.",
+		"A Vanilla JS-powered HTML calculator that performs matrix computations based on user input.",
+		"My Java solutions to the HackerRank ProjectEuler+ competition problems.",
+		"My C++ solutions to daily coding problems",
+	]
+	const GHLinks = [
+		"https://github.com/troyfeng116/chess-backtracking",
+		"https://github.com/troyfeng116/sorting-visualizer",
+		"https://github.com/troyfeng116/matrix-operations",
+		"https://github.com/troyfeng116/troy-feng-project-euler",
+		"https://github.com/troyfeng116/daily-problem",
+	]
+	
+	const projectItems = urls.map((url, index) => {
+		return (
+			<article className="card">
+				<header className="card-header">
+					<a href={url}>
+						<h2>{titles[index]}</h2>
+						<div className="card-image-container">
+							<img src={imgs[index]} className="card-image"/>
+						</div>
+					</a>
+					<p>
+						{subtext[index]}
+					</p>
+					<p>
+						<a href={GHLinks[index]}>
+							<FaGithub /> GitHub
+						</a>
+					</p>
+				</header>
+			</article>
+		)
+	})
     return (
         <section className="card-container">
-		<article className="card">
-			<header className="card-header">
-				<a href="https://troyfeng116.github.io/chess-backtracking/index.html">
-					<h2>Chess Backtracking Visualizers</h2>
-					<div className="card-image-container">
-						<img src="../media/n-queens-demo.png" className="card-image"/>
-						<img src="../media/knights-tour-demo.png" className="card-image"/>
-					</div>
-				</a>
-				<p>
-					A Vanilla JavaScript interactive visualizer for the classic N Queens and Knight's Tour
-					puzzles.
-				</p>
-				<p>
-					<a href="https://github.com/troyfeng116/chess-backtracking">
-						<FaGithub /> GitHub
-					</a>
-				</p>
-			</header>
-		</article>
-
-		<article className="card">
-			<header className="card-header">
-				<a href="https://troyfeng116.github.io/sorting-visualizer">
-					<h2>Sorting Visualizer</h2>
-					<div className="card-image-container">
-						<img src="../media/sorting-visualizer-demo-img.png" className="card-image"/>
-					</div>
-				</a>
-				<p>
-					A React+D3 Typescript web app visualizer for common sorting algorithms.
-				</p>
-				<p>
-					<a href="https://github.com/troyfeng116/sorting-visualizer">
-                    <FaGithub /> GitHub
-					</a>
-				</p>
-			</header>
-		</article>
-
-		<article className="card">
-			<header className="card-header">
-				<a href="https://troyfeng116.github.io/matrix-operations/index.html">
-					<h2>Matrix Operations Calculator</h2>
-				</a>
-                <p>
-					<a href="https://github.com/troyfeng116/mastrix-operations">
-                    <FaGithub /> GitHub
-					</a>
-				</p>
-			</header>
-		</article>
-
-		<article className="card">
-			<header className="card-header">
-				<a href="">
-					<h2>Project Euler</h2>
-				</a>
-			</header>
-		</article>
-
-		<article className="card">
-			<header className="card-header">
-				<a href="">
-					<h2>Daily Coding Problem</h2>
-				</a>
-			</header>
-		</article>
-	</section>
+			{projectItems}
+		</section>
     )
 }
