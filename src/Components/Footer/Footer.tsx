f3c1ff6841dfa28bf8a1c53ab89b2178be9b89c1
Troy Feng <troy.feng@yale.edu>
2020-11-04 00:10:46 -0500

    Footer design update

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,31 +1,63 @@
 import './Footer.css'
 import React from 'react'
 import { FaInstagramSquare, FaFacebook, FaLinkedin } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 export const Footer = () => {
     return (
         <footer className='footer-container'>
-            <header className='footer-left'>
-                Troy Feng
-            </header>
+            <nav className='footer-left'>
+                <header className='footer-logo'>
+                    TF
+                </header>
+                <section className='footer-left-column'>
+                    <Link to='/home' className='footer-left-link-wrapper'>
+                        <a className='footer-left-link'>
+                            Home
+                        </a>
+                    </Link>
+                    <Link to='/About' className='footer-left-link-wrapper'>
+                        <a className='footer-left-link'>
+                            About
+                        </a>
+                    </Link>
+                    <Link to='/Projects' className='footer-left-link-wrapper'>
+                        <a className='footer-left-link'>
+                            Projects
+                        </a>
+                    </Link>
+                </section>
+                <section className='footer-left-column'>
+                    <Link to='/other' className='footer-left-link-wrapper'>
+                        <a className='footer-left-link'>
+                            Other
+                        </a>
+                    </Link>
+                    <Link to='/contact' className='footer-left-link-wrapper'>
+                        <a className='footer-left-link'>
+                            Contact
+                        </a>
+                    </Link>
+                </section>
+            </nav>
             <nav className='footer-right'>
                 <a 
                     href='https://www.instagram.com/tro_yfeng/?hl=en'
-                    className='footer-link'
+                    className='footer-social-link'
                     target='_blank'
                 >
                     <FaInstagramSquare />
                 </a>
                 <a
                     href='https://www.facebook.com/Tfunk116'
-                    className='footer-link'
+                    className='footer-social-link'
                     target='_blank'
                 >
                     <FaFacebook />
                 </a>
                 <a
                     href='https://www.linkedin.com/in/troy-feng/'
-                    className='footer-link'
+                    className='footer-social-link'
                     target='_blank'
                 >
                     <FaLinkedin />
