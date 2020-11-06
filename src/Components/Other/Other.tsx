c8f065b50145b2459763e248447d36fe4486ee09
Troy Feng <troy.feng@yale.edu>
2020-11-06 14:42:21 -0500

    Import media, set up /other

--- src/Components/Other/Other.tsx
+++ src/Components/Other/Other.tsx
@@ -1,9 +1,25 @@
+import './Other.css'
 import React from 'react'
 
 export const Other = () => {
+    // MUSIC
+    const musicURLS = [
+        'Media/Videos/Music/Shostakovich.mp4',
+        'Media/Videos/Music/ChopinEtudeOp25N1.mp4',
+    ]
+    const musicBoard = (
+        musicURLS.map((url) => (
+            <video className='other-video' controls>
+                <source src={url} type='video/mp4' />
+            </video>
+        ))
+    )
+    // TENNIS
     return (
-        <div>
-            OTHER
+        <div className='other-container'>
+            <section className='other-section'>
+                {musicBoard}
+            </section>
         </div>
     )
 }
