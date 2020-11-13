2cb25e9a20b0533e8fd1497a241d37cb0b057bcd
Troy Feng <troy.feng@yale.edu>
2020-11-13 12:37:06 -0500

    Remove color state from Home, add alternating section

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -4,23 +4,17 @@ import { HomeSection } from './HomeSection/HomeSection'
 
 export const Home = () => {
     const [wordIndex, setWordIndex] = useState<number>(0)
-    const [colorIndex, setColorIndex] = useState<number>(0)
     const meWords = [
-        'probem solver',
+        'problem solver',
         'performer',
         'entertainer',
-    ]
-    const colorArr = [
-        'red',
-        'green',
-        'blue',
-        'pink',
-        'orange',
+        'programmer',
+        'tenista',
+        'pianist',
     ]
 
     useEffect(() => {
         const timeOut = setTimeout(() => {
-            setColorIndex((colorIndex + 1) % colorArr.length)
             setWordIndex((wordIndex + 1) % meWords.length)
         }, 2000)
         return () => clearTimeout(timeOut)
@@ -28,6 +22,7 @@ export const Home = () => {
 
     const textArr = [
         `I am currently an undergraduate student at Yale University in Pauli Murray College, class of 2023.5 as I am on a leave of absence after my first year (Class of 2023 at heart). I will hopefully be double majoring in Computer Science & Mathematics (B.S.) and Physics (B.A.), and I am interested in AI and quantum computing. I have also already taken or plan to take coursework in music performance (private lessons and chamber music), Chinese (advanced language and history/culture), philosophy, and art history. On campus, I work as a peer tutor in the Math Department, I volunteer with Code Haven as a classroom mentor, and I am a problem writer for Yale Math Competition.`,
+        `I am a Software Engineer Intern at Eureka Surveys.`
     ]
     const imgUrls = [
         [
@@ -36,6 +31,10 @@ export const Home = () => {
             'Media/Images/MurrayTower3.jpg',
             'Media/Images/MurrayTower4.jpg',
             'Media/Images/MurrayTower5.jpg',
+        ],
+        [
+            'Media/Images/Math.jpg',
+            'Media/Images/leaning-back.jpg',
         ],
     ]
     const homeSections = textArr.map((text, index) => {
@@ -53,14 +52,17 @@ export const Home = () => {
             <section className='home-bio-container'>
                 <div className='home-bio-img-outer-container'>
                     <figure className='home-bio-img-container'>
-                        <img className='home-bio-img' src='Media/Images/MugShot.jpg' />
+                        <img className='home-bio-img' src='Media/Images/MugShot.jpg' alt='Troy Feng' />
                     </figure>
                 </div>
                 <div className='home-bio-text-container'>
-                    <h2>Hello.</h2>
+                    <h2>Hello</h2>
                     <p>
                         My name is Troy Feng, and I am a&nbsp;
-                        <span className='home-bio-me-word' style={{ color: colorArr[colorIndex] }}>
+                        <span
+                            className='home-bio-me-word'
+                            style={{ color: '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0') }}
+                        >
                             {meWords[wordIndex]}
                         </span>
                     </p>
