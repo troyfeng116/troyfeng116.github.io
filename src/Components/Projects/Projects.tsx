eda1da8fc78159bc9f4d28f0a28f6d1706f69f9f
Troy Feng <troy.feng@yale.edu>
2020-12-12 12:36:26 -0500

    Add Projects media

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -26,18 +26,31 @@ export const Projects = () => {
 	]
 	const imgs = [
 		[
-			'Media/Images/Projects/n-queens-demo.png',
-			'/Media/Images/Projects/knights-tour-demo.png'
+			'Media/Images/Projects/chess-backtracking/n-queens-demo.png',
+			'Media/Images/Projects/chess-backtracking/knights-tour-demo.png',
 		],
-		['Media/Images/Projects/merge-sort.png'],
 		[
-			'Media/Images/Projects/multiplication.png',
-			'Media/Images/Projects/determinant.png',
-			'Media/Images/Projects/rref.png',
-			'Media/Images/Projects/inverse.png'
+			'Media/Images/Projects/sorting-visualizer/sorting-visualizer-demo.png',
+			'Media/Images/Projects/sorting-visualizer/bsort.png',
+			'Media/Images/Projects/sorting-visualizer/isort.png',
+			'Media/Images/Projects/sorting-visualizer/hsort.png',
+			'Media/Images/Projects/sorting-visualizer/qsort.png',
+			'Media/Images/Projects/sorting-visualizer/msort.png',
 		],
-		['Media/Images/Projects/sorting-visualizer-demo-img.png'],
-		['Media/Images/Projects/sorting-visualizer-demo-img.png'],
+		[
+			'Media/Images/Projects/matrix-operations/multiplication.png',
+			'Media/Images/Projects/matrix-operations/determinant.png',
+			'Media/Images/Projects/matrix-operations/rref.png',
+			'Media/Images/Projects/matrix-operations/inverse.png',
+		],
+		[
+			'Media/Images/Projects/project-euler-1.png',
+			'Media/Images/Projects/project-euler-2.png',
+		],
+		[
+			'Media/Images/Projects/daily-problem-1.png',
+			'Media/Images/Projects/daily-problem-2.png',
+		],
 		['Media/Images/Projects/sorting-visualizer-demo-img.png'],
 	]
 	const subtext = [
@@ -45,7 +58,7 @@ export const Projects = () => {
 		"A React+D3 Typescript web app visualizer for common sorting algorithms.",
 		"A VanillaJS-powered calculator that performs matrix computations based on user input.",
 		"My Java solutions to the HackerRank ProjectEuler+ competition problems.",
-		"My C++ solutions to daily coding problems",
+		"My C++11 solutions to some daily coding and algorithm problems",
 		"In order to understand recursion, one must first understand recursion.",
 	]
 	const GHLinks = [
@@ -60,52 +73,48 @@ export const Projects = () => {
 	const projectItems = urls.map((url, index) => {
 		return (
 			<section className="card" key={`project-${index}`}>
-				<ProjectsAnimation>
-					<section className="card-content">
-						<a href={url} target="_blank" rel="noopener noreferrer">
-							<h2>{titles[index]}</h2>
+				<section className="card-content">
+					<a href={url} target="_blank" rel="noopener noreferrer">
+						<h2>{titles[index]}</h2>
+					</a>
+					<div className="card-image-container">
+						{imgs[index].length > 1 ?
+							(
+								<Gallery
+									items={
+										imgs[index].map((item) => (
+											<img
+												src={item}
+												className='card-image'
+												onClick={() => setShowImageModal(item)}
+												alt={'Troy Feng - project sample unavailable'}
+											/>
+										))
+									}
+									maxHeight={300}
+									showCircles={false}
+									arrowsInside={true}
+									timeoutSeconds={Math.random() * 5 + 8}
+								/>
+							) : (
+								<img
+									src={imgs[index][0]}
+									className='card-image'
+									onClick={() => setShowImageModal(imgs[index][0])}
+									alt={'Troy Feng - project sample unavailable'}
+								/>
+							)
+						}
+					</div>
+					<a href={url} target='_blank' rel="noopener noreferrer">
+						{subtext[index]}
+					</a>
+					<p>
+						<a href={GHLinks[index]} target="_blank" rel="noopener noreferrer">
+							<FaGithub /> GitHub
 						</a>
-						<div className="card-image-container">
-							{imgs[index].length > 1 ?
-								(
-									<Gallery
-										items={
-											imgs[index].map((item) => {
-												return (
-													<img
-														src={item}
-														className='card-image'
-														onClick={() => setShowImageModal(item)}
-														alt={'Troy Feng - project sample unavailable'}
-													/>
-												)
-											})
-										}
-										maxHeight={300}
-										showCircles={false}
-										arrowsInside={true}
-										timeoutSeconds={Math.random() * 5 + 8}
-									/>
-								) : (
-									<img
-										src={imgs[index][0]}
-										className='card-image'
-										onClick={() => setShowImageModal(imgs[index][0])}
-										alt={'Troy Feng - project sample unavailable'}
-									/>
-								)
-							}
-						</div>
-						<a href={url} target='_blank' rel="noopener noreferrer">
-							{subtext[index]}
-						</a>
-						<p>
-							<a href={GHLinks[index]} target="_blank" rel="noopener noreferrer">
-								<FaGithub /> GitHub
-							</a>
-						</p>
-					</section>
-				</ProjectsAnimation>
+					</p>
+				</section>
 			</section>
 		)
 	})
