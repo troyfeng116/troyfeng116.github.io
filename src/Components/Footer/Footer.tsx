b87e6a9760271750daea7e88762cac6f9ba094c8
Troy Feng <troy.feng@yale.edu>
2020-12-19 15:17:25 -0500

    Cleanup Contact and Footer components

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -6,64 +6,64 @@ import TextMultiColor from '../TextMultiColor/TextMultiColor'
 
 export const Footer: React.FC = () => {
     return (
-        <footer className='footer-container'>
-            <nav className='footer-left'>
-                <Link to='/' className='footer-logo-wrapper'>
-                    <header className='footer-logo'>
+        <footer className="footer-container">
+            <nav className="footer-left">
+                <Link to="/" className="footer-logo-wrapper">
+                    <header className="footer-logo">
                         <TextMultiColor text="TF" />
                     </header>
                 </Link>
-                <section className='footer-left-column'>
-                    <Link to='/home' className='footer-left-link-wrapper'>
-                        <div className='footer-left-link'>
+                <section className="footer-left-column">
+                    <Link to="/home" className="footer-left-link-wrapper">
+                        <div className="footer-left-link">
                             Home
                         </div>
                     </Link>
-                    <Link to='/About' className='footer-left-link-wrapper'>
-                        <div className='footer-left-link'>
+                    <Link to="/About" className="footer-left-link-wrapper">
+                        <div className="footer-left-link">
                             About
                         </div>
                     </Link>
-                    <Link to='/Projects' className='footer-left-link-wrapper'>
-                        <div className='footer-left-link'>
+                    <Link to="/Projects" className="footer-left-link-wrapper">
+                        <div className="footer-left-link">
                             Projects
                         </div>
                     </Link>
                 </section>
-                <section className='footer-left-column'>
-                    <Link to='/other' className='footer-left-link-wrapper'>
-                        <div className='footer-left-link'>
+                <section className="footer-left-column">
+                    <Link to="/other" className="footer-left-link-wrapper">
+                        <div className="footer-left-link">
                             Other
                         </div>
                     </Link>
-                    <Link to='/contact' className='footer-left-link-wrapper'>
-                        <div className='footer-left-link'>
+                    <Link to="/contact" className="footer-left-link-wrapper">
+                        <div className="footer-left-link">
                             Contact
                         </div>
                     </Link>
                 </section>
             </nav>
-            <nav className='footer-right'>
+            <nav className="footer-right">
                 <a
-                    href='https://www.instagram.com/tro_yfeng/?hl=en'
-                    className='footer-social-link'
-                    target='_blank'
+                    href="https://www.instagram.com/tro_yfeng/?hl=en"
+                    className="footer-social-link"
+                    target="_blank"
                     rel="noopener noreferrer"
                 >
                     <FaInstagramSquare />
                 </a>
                 <a
-                    href='https://www.facebook.com/Tfunk116'
-                    className='footer-social-link'
-                    target='_blank'
+                    href="https://www.facebook.com/Tfunk116"
+                    className="footer-social-link"
+                    target="_blank"
                     rel="noopener noreferrer"
                 >
                     <FaFacebook />
                 </a>
                 <a
-                    href='https://www.linkedin.com/in/troy-feng/'
-                    className='footer-social-link'
-                    target='_blank'
+                    href="https://www.linkedin.com/in/troy-feng/"
+                    className="footer-social-link"
+                    target="_blank"
                     rel="noopener noreferrer"
                 >
                     <FaLinkedin />
