82b3a4efcca5d00130520dbd123564b9f0a7f835
Troy Feng <troy.feng@yale.edu>
2020-12-19 14:39:36 -0500

    Import music media, update other display

--- src/Components/About/AboutConstants.ts
+++ src/Components/About/AboutConstants.ts
@@ -1,11 +1,76 @@
 export const aboutTextArr = [
-    `My name is Troy, and I'm originally from Rhode Island. I'm a long-time problem solver, and I enjoy mathematics (number theory, probability, & statistics), computer programming (web development & algorithmic computation), and quantum physics. I also love learning, teaching, and sharing knowledge. When I'm not busy studying or working, I'm most likely out with friends, playing the piano, playing tennis, watching various TV shows (both American and Chinese), or watching sports (I'm a fan of the Boston sports teams, and I also follow the ATP/WTA and swimming).`,
-    `I am currently an undergraduate student at Yale University in Pauli Murray College, class of 2024 as I am on a leave of absence after my first year (Class of 2023 always at heart). I will be double majoring in Computer Science & Mathematics (B.S.) and Physics (B.A.), and I am interested in AI and quantum computing. I have also already taken and plan to take more coursework in music performance (private lessons and chamber music), Chinese (advanced language and history/culture), philosophy, and art history. On campus, I work as a peer tutor in the Math Department, I volunteer with Code Haven as a classroom mentor, and I am a problem writer for Yale Math Competition.`,
-    `I love building and perfecting products, and I am an experienced React TypeScript web developer with the ability to efficiently build and ship industry-standard code and products within an inter-departmental team. I am currently a Software Engineer Intern at Eureka Surveys, a market research startup that distributes paid surveys to over 50,000 daily active users. At Eureka, I am a frontend engineer working in React Typescript and Next.js, building substantial web product features from the ground-up including the company landing page, blog, and survey infrastructure. My frontend knowledge and skillset also includes data visualization, UX product design, SEO, server side rendering, website optimization, and analytics.`,
-    `I have an extensive problem-solving background, thanks to my years of experience in competitive mathematics, academic coursework, and personal technical projects. I have substantial knowledge and experience in Java/C/C++, algorithms and data structures, number theory, probability, statistics, numerical and algorithmic computation, and combinatorics. My personal projects include a backtracking algorithm visualizer for chess puzzles, a sorting algorithm visualizer, and 100+ computational number theory problems solved in HackerRank's ProjectEuler+ competition. I hope to combine my love of numbers and problem solving with my passion for building to conduct research at the intersection of computer science, mathematics, and physics.`,
-    `Teamwork and collaboration are of the utmost importance to me in everything that I do. I enjoy teaching as much as I enjoy learning, and I am an efficient and enthusiastic communicator when it comes to sharing knowledge. I have held various tutoring and mentorship positions at Juni Learning and Yale, which have been rewarding and valuable experiences for me and (I hope) the people I met. Be it in the context of software engineering, playing sports, or gruelling problem sets, I hold a deep investment in the success of my peers and teammates. I understand that I can only perform at my best in an environment where those around me are also as invested in my success as their own, and I constantly and actively seek such environments.`,
-    `I have been playing the piano for 14 years, and I am an experienced performer. Much of my formal performance experience is as a classical pianist: I am a two-time winner (2015, 2017) of the Rhode Island Philharmonic Concerto & Aria Competition, 2nd (2018), 3rd (2017), and 1st (2015) place finisher at the URI Piano Extravaganza!, and an Honors Recital invitee at the RIMEA Solo & Ensemble Festival (2019). In addition to my classical training, I also have experience as a jazz pianist, jazz vibrophonist, and chamber pianist. Some of my personal favorite piano repertoire includes the Chopin Ballades, Liszt's La Campanella, Keith Jarret's Köln Concert, and Brahms's 2nd Piano Concerto.`,
-    `Much of my skills in discipline, focus, time management, teamwork, and leadership can be attributed to playing sports. During my career as a five-year high school varsity swimmer, four-year high school varsity tennis player, eight-year USA Swimming club swimmer, and eight-year USTA tennis player, I met and trained with many of my closest friends to date. Being with my teammates pushed me to 3 RIIL All-State honors, 9 RIIL All-Division honors, and 7 top 30 event season finishes in the New England Swimming age group rankings. While my days as a formal tennis player and swimmer are likely over, I still frequently hit the pool and courts to spend time with friends and loosen up in my free time.`,
+    `My name is Troy, and I'm originally from Rhode Island.
+I'm a long-time problem solver, and I enjoy mathematics
+(number theory, probability, & statistics), computer programming
+(web development & algorithmic computation), and quantum physics.
+I also love learning, teaching, and sharing knowledge. When I'm not
+busy studying or working, I'm most likely out with friends, playing
+the piano, playing tennis, watching various TV shows (both American
+and Chinese), or watching sports (I'm a fan of the Boston sports 
+teams, and I also follow the ATP/WTA and swimming).`,
+    `I am currently an undergraduate student at Yale University in
+Pauli Murray College, class of 2024 as I am on a leave of absence after
+my first year (Class of 2023 always at heart). I will be double majoring
+in Computer Science & Mathematics (B.S.) and Physics (B.A.), and I am
+interested in AI and quantum computing. I have also already taken and
+plan to take more coursework in music performance (private lessons and
+chamber music), Chinese (advanced language and history/culture),
+philosophy, and art history. On campus, I work as a peer tutor in the
+Math Department, I volunteer with Code Haven as a classroom mentor,
+and I am a problem writer for Yale Math Competition.`,
+    `I love building and perfecting products, and I am an experienced
+React TypeScript web developer who can efficiently build and ship
+industry-standard code and products within an inter-departmental team.
+I am currently a Software Engineer Intern at Eureka Surveys, a market 
+research startup that distributes paid surveys to over 50,000 daily
+active users. At Eureka, I am a frontend engineer working in React
+Typescript and Next.js, building substantial web product features from
+the ground-up including the company landing page, blog, and survey
+infrastructure. My frontend knowledge and skillset also includes data
+visualization, UX product design, SEO, server side rendering, website
+optimization, and analytics.`,
+    `I have an extensive problem-solving background, thanks to my years
+of experience in competitive mathematics, academic coursework, and
+personal technical projects. I have substantial knowledge and experience
+in Java/C/C++, algorithms and data structures, number theory, probability,
+statistics, numerical and algorithmic computation, and combinatorics.
+My personal projects include a backtracking algorithm visualizer for
+chess puzzles, a sorting algorithm visualizer, and 100+ computational
+number theory problems solved in HackerRank's ProjectEuler+ competition.
+I hope to combine my love of numbers and problem solving with my passion
+for building to conduct research at the intersection of computer science,
+mathematics, and physics.`,
+    `Teamwork and collaboration are of the utmost importance to me in
+everything that I do. I enjoy teaching as much as I enjoy learning,
+and I am an efficient and enthusiastic communicator when it comes to
+sharing knowledge. I have held various tutoring and mentorship positions
+at Juni Learning and Yale, which have been rewarding and valuable
+experiences for me and (I hope) the people I met. Be it in the context
+of software engineering, playing sports, or gruelling problem sets,
+I hold a deep investment in the success of my peers and teammates.
+I understand that I can only perform at my best in an environment
+where those around me are also as invested in my success as their own,
+and I constantly and actively seek such environments.`,
+    `I have been playing the piano for 14 years, and I am an
+experienced performer. Much of my formal performance experience is
+as a classical pianist: I am a two-time winner (2015, 2017) of the
+Rhode Island Philharmonic Concerto & Aria Competition, 2nd (2018),
+3rd (2017), and 1st (2015) place finisher at the URI Piano Extravaganza!,
+and an Honors Recital invitee at the RIMEA Solo & Ensemble Festival (2019).
+In addition to my classical training, I also have experience as a
+jazz pianist, jazz vibrophonist, and chamber pianist. Some of my
+personal favorite piano repertoire includes the Chopin Ballades,
+Liszt's La Campanella, Keith Jarret's Köln Concert, and Brahms's 2nd Piano Concerto.`,
+    `Much of my skills in discipline, focus, time management, teamwork,
+and leadership can be attributed to playing sports. During my career as
+a five-year high school varsity swimmer, four-year high school varsity
+tennis player, eight-year USA Swimming club swimmer, and eight-year USTA
+tennis player, I met and trained with many of my closest friends to date.
+Being with my teammates pushed me to 3 RIIL All-State honors,
+9 RIIL All-Division honors, and 7 top 30 event season finishes in the
+New England Swimming age group rankings. While my days as a formal
+tennis player and swimmer are likely over, I still frequently hit the pool
+and courts to spend time with friends and loosen up in my free time.`,
 ]
 
 export const aboutImgUrls = [
--- src/Components/Other/Other.tsx
+++ src/Components/Other/Other.tsx
@@ -3,48 +3,77 @@ import React from 'react'
 
 export const Other = () => {
     // MUSIC
-    const musicURLs = [
+    const videoURLs = [
         'w2ldAb6jKMk',
         'fHjkQ3vkRQI',
         '-HbkyJ1RVDA',
+        'LrvYGi9WWcc',
+        'ALKK-PzimYI',
+        '-IeaY9Xci4k',
     ]
-    const musicTitles = [
+    const videoTitles = [
         'Chopin Scherzo No. 3 in C# minor, Op. 39',
         'Shostakovich Piano Concerto No. 2 in F major, Op. 102',
-        'Chopin Étude No. 1 in A-flat major, Op. 25'
+        'Chopin Étude No. 1 in A-flat major, Op. 25',
+        'Scriabin Etude Op. 5 in C# minor, Op. 42',
+        'Chopin Scherzo No. 4 in E major, Op. 54',
+        'Rachmaninoff Etude-Tableaux No. 5 in E-flat minor, Op. 39',
     ]
-    const musicBoard = (
-        musicURLs.map((url, i) => (
-            <div className='other-section-item' key={i}>
+    const videoBoard = (
+        videoURLs.map((url, i) => (
+            <div className="other-section-item" key={i}>
                 <iframe
                     className="other-video"
-                    //width="560"
-                    //height="250"
                     src={`https://www.youtube.com/embed/${url}`}
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                     color="white"
                 />
-                <p className='other-section-label'>
-                    {musicTitles[i]}
+                <p className="other-section-label">
+                    {videoTitles[i]}
                 </p>
+            </div>
+        ))
+    )
+
+    const audioURLs = [
+        'Media/Videos/stravinsky-trio.mp3',
+        'Media/Videos/grande-polonaise.mp3',
+        'Media/Videos/Dumka.mp3',
+        'Media/Videos/BachPF.mp3',
+        'Media/Videos/ItalianConcertoMvt1.mp3',
+        'Media/Videos/BeethovenSonataAudio.mp3',
+    ]
+    const audioTitles = [
+        'Stravinsky L\'Histoire du Soldat, violin + clarinet + piano trio',
+        'Chopin Grande Polonaise Brillante in E-flat major, Op. 22',
+        'Tchaikovsky Dumka in C minor, Op. 53',
+        'Bach Prelude and Fugue No. 10 in E minor, BWV 855',
+        'Bach Italian Concerto, BWV 971, 1st movement',
+        'Beethoven Sonata No. 3 in C major, Op. 2',
+    ]
+    const audioBoard = (
+        audioURLs.map((url, index) => (
+            <div className="other-section-item other-section-item-audio">
+                <audio controls>
+                    <source src={url} type="audio/mp3" />
+                </audio>
+                <p className="other-section-label">{audioTitles[index]}</p>
             </div>
         ))
     )
     // TENNIS
 
     return (
-        <div className='other-container'>
-            <section className='other-section'>
-                <h3 className='other-section-title'>
+        <div className="other-container">
+            <section className="other-section">
+                <h3 className="other-section-title">
                     Musician, Pianist, Performer.
                 </h3>
-                <section className='other-section-grid'>
-                    {musicBoard}
-                    <audio controls>
-                        <source src='Media/Videos/Dumka.mp3' type='audio/mp3' />
-                    </audio>
+                <section className="other-section-grid">
+                    {videoBoard}
+                    {audioBoard}
                 </section>
             </section>
         </div>
