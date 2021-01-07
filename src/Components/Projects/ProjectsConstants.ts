002dcb11313322a2fff1afc1cfa6cb3d61f8ae89
Troy Feng <troy.feng@yale.edu>
2021-01-07 14:12:35 -0500

    Update projects

--- src/Components/Projects/ProjectsConstants.ts
+++ src/Components/Projects/ProjectsConstants.ts
@@ -3,11 +3,12 @@ export const projectUrls = [
     'https://troyfeng116.github.io/sorting-visualizer',
     'https://troyfeng116.github.io/matrix-operations/index.html',
     'https://github.com/troyfeng116/troy-feng-project-euler',
+    'https://troyfeng116.github.io/web-counter',
     'https://github.com/troyfeng116/daily-problem',
     '/projects',
 ]
 
-export const projectTitles = ['Chess Backtracking Visualizers', 'Sorting Visualizer', 'Matrix Operations Calculator', 'Project Euler+', 'Daily Coding Problem', 'Personal Website']
+export const projectTitles = ['Chess Backtracking Visualizers', 'Sorting Visualizer', 'Matrix Operations Calculator', 'Project Euler+', 'Web Counter', 'Daily Coding Problem', 'Personal Website']
 
 export const projectImgs = [
     ['Media/Images/Projects/chess-backtracking/n-queens-demo.png', 'Media/Images/Projects/chess-backtracking/knights-tour-demo.png'],
@@ -26,15 +27,17 @@ export const projectImgs = [
         'Media/Images/Projects/matrix-operations/inverse.png',
     ],
     ['Media/Images/Projects/project-euler/project-euler-1.png', 'Media/Images/Projects/project-euler/project-euler-2.png'],
+    ['Media/Images/Projects/web-counter/web-counter-1.png', 'Media/Images/Projects/web-counter/web-counter-2.png'],
     ['Media/Images/Projects/daily-problem/daily-problem-1.png', 'Media/Images/Projects/daily-problem/daily-problem-2.png'],
     ['Media/Images/Projects/personal-website-img.png'],
 ]
 
 export const projectSubtext = [
     "A VanillaJS interactive visualizer for the classic N Queens and Knight's Tour puzzles.",
-    'A React+D3 Typescript web app visualizer for common sorting algorithms.',
+    'A React+D3 TypeScript web app visualizer for common sorting algorithms.',
     'A VanillaJS-powered calculator that performs matrix computations based on user input.',
     'My Java solutions to the HackerRank ProjectEuler+ competition problems.',
+    'A React TypeScript + TailwindCSS simple web tally counter.',
     'My C++11 solutions to some daily coding and algorithm problems',
     'In order to understand recursion, one must first understand recursion.',
 ]
@@ -44,6 +47,7 @@ export const projectGHLinks = [
     'https://github.com/troyfeng116/sorting-visualizer',
     'https://github.com/troyfeng116/matrix-operations',
     'https://github.com/troyfeng116/troy-feng-project-euler',
+    'https://github.com/troyfeng116/web-counter',
     'https://github.com/troyfeng116/daily-problem',
     'https://github.com/troyfeng116/troyfeng116.github.io',
 ]
