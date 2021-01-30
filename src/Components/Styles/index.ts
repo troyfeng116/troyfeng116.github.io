471dfbb943641a17829a284cfb4dfa2377351843
Troy Feng <troy.feng@yale.edu>
2021-01-30 13:43:28 -0500

    Standard styles

---
+++ src/Components/Styles/index.ts
@@ -0,0 +1,9 @@
+export enum Fonts {
+    SmallText = 'standard-fonts-small-text',
+    MediumText = 'standard-fonts-medium-text',
+    LargeText = 'standard-fonts-large-text',
+}
+
+export enum TextColors {
+    Purple = 'standard-text-colors-purple',
+}
