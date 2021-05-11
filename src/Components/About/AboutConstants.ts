f9350a864a24e30d6a5bc4b584516d8aaa8834f1
Troy Feng <troy.feng@yale.edu>
2021-05-11 14:56:33 -0400

    Update about constants

--- src/Components/About/AboutConstants.ts
+++ src/Components/About/AboutConstants.ts
@@ -2,31 +2,33 @@ export const aboutTextArr = [
     `My name is Troy, and I'm originally from Rhode Island.
 I'm a long-time problem solver, and I enjoy mathematics
 (number theory, probability, & statistics), computer programming
-(web development & algorithmic computation), and quantum physics.
+(web development & algorithmic computation), and philosophy.
 I also love learning, teaching, and sharing knowledge. When I'm not
 busy studying or working, I'm most likely out with friends, playing
 the piano, playing tennis, watching various TV shows (both American
-and Chinese), or watching sports (I'm a fan of the Boston sports 
-teams, and I also follow the ATP/WTA and swimming).`,
+and Chinese), or watching sports (I'm a huge fan of the ATP/WTA and 
+swimming, and I also love the Boston sports teams).`,
     `I am currently an undergraduate student at Yale University in
 Pauli Murray College, class of 2024 as I am on a leave of absence after
 my first year (Class of 2023 always at heart). I will be majoring
-in Computer Science & Mathematics (B.S.) and Physics (B.A.), and I am
+in Computer Science & Mathematics (B.S.) and Philosophy (B.A. with a
+concentration in psychology), and I am
 interested in AI and quantum computing. I have also already taken and
 plan to take more coursework in music performance (private lessons and
 chamber music), Chinese (advanced language and history/culture),
-philosophy, and art history. On campus, I work as a peer tutor in the
-Math Department, I volunteer with Code Haven as a classroom mentor,
-and I am a problem writer for Yale Math Competition.`,
+sociology, and art history. On campus, you can usually probably find me
+searching for new study spots, leading office hours and section as a 
+mathematics peer tutor and CS50 TA, volunteering with Code Haven as
+a classroom mentor, or practicing in the Addams Center or Pauli Murray
+piano rooms.`,
     `I love building and perfecting products, and I am an experienced
 React TypeScript web developer who can efficiently build and ship
 industry-standard code and products within an inter-departmental team.
 I am currently a Software Engineer Intern at Eureka Surveys, a market 
 research startup that distributes paid surveys to over 50,000 daily
 active users. At Eureka, I am a frontend engineer working in React
-Typescript and Next.js, building substantial web product features from
-the ground-up including the company landing page, blog, and survey
-infrastructure. My frontend knowledge and skillset also includes data
+Typescript and Next.js, building and overseeing all web product engineering from
+the ground-up. My frontend knowledge and skillset also includes data
 visualization, UX product design, SEO, server side rendering, website
 optimization, and analytics.`,
     `I have an extensive problem-solving background, thanks to my years
@@ -38,17 +40,18 @@ My personal projects include a backtracking algorithm visualizer for
 chess puzzles, a sorting algorithm visualizer, and 100+ computational
 number theory problems solved in HackerRank's ProjectEuler+ competition.
 I hope to combine my love of numbers and problem solving with my passion
-for building to conduct research at the intersection of computer science,
-mathematics, and physics.`,
+for building to conduct research at the intersection of computer science and
+mathematics, namely in natural language and computational linguistics with
+applications in behavioral economics and sociology.`,
     `Teamwork and collaboration are of the utmost importance to me in
 everything that I do. I enjoy teaching as much as I enjoy learning,
-and I am an efficient and enthusiastic communicator when it comes to
+and I am an enthusiastic communicator when it comes to
 sharing knowledge. I have held various tutoring and mentorship positions
 at Juni Learning and Yale, which have been rewarding and valuable
 experiences for me and (I hope) the people I've met. Be it in the context
-of software engineering, playing sports, or gruelling problem sets,
-I hold a deep investment in the success of my peers and teammates.
-I understand that I can only perform at my best in an environment
+of software engineering, playing sports, or slogging through gruelling problem sets,
+I am deeply invested in the success of my peers and teammates.
+I know that I can only perform at my best in an environment
 where those around me are also as invested in my success as their own,
 and I constantly and actively seek such environments.`,
     `I have been playing the piano for 14 years, and I am an
@@ -63,7 +66,7 @@ personal favorite piano repertoire includes the Chopin Ballades,
 Liszt's La Campanella, Keith Jarret's Köln Concert, and Brahms's 2nd
 Piano Concerto.`,
     `Much of my skills in discipline, focus, time management, teamwork,
-and leadership can be attributed to playing sports. During my career as
+and leadership can be attributed to sports. During my career as
 a five-year high school varsity swimmer, four-year high school varsity
 tennis player, eight-year USA Swimming club swimmer, and eight-year USTA
 tennis player, I met and trained with many of my closest friends to date.
