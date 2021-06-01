ce0e26afcae3010f1425b64afcba795cc405d2f0
Troy Feng <troy.feng@yale.edu>
2021-05-31 20:44:28 -0400

    Collapsible component + experience section cleanup

--- src/Components/About/ExperienceCell/ExperienceCell.tsx
+++ src/Components/About/ExperienceCell/ExperienceCell.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { StandardFlex, StandardFonts, StandardMargin, StandardWidth } from '../../Styles'
+import Collapsible from '../../Collapsible'
+import { StandardFlex, StandardFonts, StandardMargin, StandardPadding, StandardTextColors, StandardWidth } from '../../Styles'
 import TextGradient from '../../TextGradient'
 
 export interface ExperienceCellProps {
@@ -18,40 +19,51 @@ export const ExperienceCell: React.FC<ExperienceCellProps> = (props) => {
     const { title, company, companyHref, timeAtPosition, description, workSampleLabel, workSampleHref, workSampleNote } = props
     return (
         <TextGradient className={`${StandardMargin.B24}`} from="#d475d4" to="#fa9f55">
-            <div>
-                <p className={`${StandardFonts.MediumTextBold} ${StandardWidth.FitContent} ${StandardMargin.T0} ${StandardMargin.B0}`}>{title}</p>
-                <p className={`${StandardFonts.SmallTextBold} ${StandardMargin.Y0} ${StandardMargin.L12}`}>
-                    {companyHref !== undefined ? (
-                        <a href={companyHref} rel="noreferrer" target="_blank" style={{ textDecoration: '#d475d4 underline' }}>
-                            {company}
-                        </a>
-                    ) : (
-                        company
-                    )}
-                    &nbsp;&nbsp;|&nbsp;&nbsp;{timeAtPosition}
-                </p>
-                <div className={`${StandardFonts.SmallTextAltBold} ${StandardMargin.T6} ${StandardMargin.L12}`}>
-                    {description.map((description, idx) => {
-                        return (
-                            <div key={idx} className={`${StandardFlex.Row}`}>
-                                <span>‣</span>
-                                <p className={`${StandardMargin.Y0} ${StandardMargin.L6}`}>{description}</p>
-                            </div>
-                        )
-                    })}
-                </div>
-                {workSampleLabel !== undefined && workSampleHref !== undefined && (
-                    <div className={`${StandardMargin.T6}`}>
-                        <p className={`${StandardFonts.SmallTextBold} ${StandardMargin.Y0} ${StandardMargin.L12}`}>
-                            Work sample:&nbsp;
-                            <a href={workSampleHref} rel="noreferrer" target="_blank" style={{ textDecoration: '#d475d4 underline' }}>
-                                {workSampleLabel}
-                            </a>
-                            {workSampleNote !== undefined && <span className={`${StandardFonts.SmallTextAltBold}`}>&nbsp;{workSampleNote}</span>}
+            <Collapsible
+                label={
+                    <div className={`${StandardPadding.Y12}`}>
+                        <p className={`${StandardFonts.MediumTextBold} ${StandardWidth.FitContent} ${StandardMargin.T0} ${StandardMargin.B0}`}>{title}</p>
+                        <p className={`${StandardFonts.SmallTextBold} ${StandardMargin.Y0}`}>
+                            {companyHref !== undefined ? (
+                                <a href={companyHref} rel="noreferrer" target="_blank" style={{ textDecoration: '#d475d4 underline' }}>
+                                    {company}
+                                </a>
+                            ) : (
+                                company
+                            )}
+                            &nbsp;&nbsp;|&nbsp;&nbsp;{timeAtPosition}
                         </p>
                     </div>
-                )}
-            </div>
+                }
+                containerClassName={`${StandardPadding.X18}`}
+                containerStyle={{ border: '2px solid #5078f0', borderRadius: 6 }}
+                chevronClassName={`${StandardTextColors.Orange}`}
+                isOnlyChevronClickable={false}
+            >
+                <div className={`${StandardFonts.SmallTextAltBold} ${StandardPadding.B18} ${StandardPadding.L12} ${StandardPadding.R24}`}>
+                    <div className={`${StandardFonts.SmallTextAltBold}`}>
+                        {description.map((desc, idx) => {
+                            return (
+                                <div key={idx} className={`${StandardFlex.Row} ${idx === description.length - 1 ? '' : StandardMargin.B6}`}>
+                                    <span>‣</span>
+                                    <p className={`${StandardMargin.Y0} ${StandardMargin.L6}`}>{desc}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    {workSampleLabel !== undefined && workSampleHref !== undefined && (
+                        <div className={`${StandardMargin.T12}`}>
+                            <p className={`${StandardMargin.Y0}`}>
+                                Work sample:&nbsp;
+                                <a href={workSampleHref} rel="noreferrer" target="_blank" style={{ textDecoration: '#d475d4 underline' }}>
+                                    {workSampleLabel}
+                                </a>
+                                {workSampleNote !== undefined && <span>&nbsp;{workSampleNote}</span>}
+                            </p>
+                        </div>
+                    )}
+                </div>
+            </Collapsible>
         </TextGradient>
     )
 }
