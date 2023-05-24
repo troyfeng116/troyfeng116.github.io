076d6eac05085861f204a9b7d504c16c67f44e8b
Troy Feng <troy.feng@yale.edu>
2023-05-23 21:23:17 -0400

    Update About

--- src/Components/About/WhoAmI/WhoAmI.tsx
+++ src/Components/About/WhoAmI/WhoAmI.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import ItemList from 'Components/About/ItemList'
 import TextGradient, { TextGradientColors } from 'Components/TextGradient'
 import { StandardFonts, StandardMargin } from 'Styles/Standard'
 import { useTextGradientThemeMap } from 'Styles/Theme/useTextGradientThemeMap'
 
 export interface WhoAmIProps {
     paragraphs: string[]
+    skills: string[]
 }
 
 export const WhoAmI: React.FC<WhoAmIProps> = (props) => {
-    const { paragraphs } = props
+    const { paragraphs, skills } = props
 
     const textGradientThemeMap = useTextGradientThemeMap()
 
@@ -26,6 +28,9 @@ export const WhoAmI: React.FC<WhoAmIProps> = (props) => {
                     {para}
                 </TextGradient>
             ))}
+            <TextGradient className={`${StandardMargin.T18}`} from={textGradientThemeMap[TextGradientColors.Pink]} to={textGradientThemeMap[TextGradientColors.Orange]}>
+                <ItemList label="Skills" items={skills} />
+            </TextGradient>
         </div>
     )
 }
